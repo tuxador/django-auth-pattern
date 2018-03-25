e593a7e23f2a9f3688c901403014e0302fa5e2dd
Yahyaoui Kaddour <kaddourkardio@gmail.com>
2018-03-25 23:19:20 +0100

    ajout de purge-all comme option

--- correspondence/urls.py
+++ correspondence/urls.py
@@ -4,13 +4,13 @@ from . import views
 
 app_name = 'correspondence'
 urlpatterns = [
-    path('<slug:slug>_courrier<int:pk>',
+    path('<slug:slug>_courrier<int:pk>.pdf',
          views.courrier_pdf, name='courrier_pdf'),
-    path('<slug:slug>_certificat<int:pk>',
+    path('<slug:slug>_certificat<int:pk>.pdf',
          views.certificat_pdf, name='certificat_pdf'),
-    path('<slug:slug>_stomato<int:pk>',
+    path('<slug:slug>_stomato<int:pk>.pdf',
          views.stomato_pdf, name='stomato_pdf'),
-    path('<slug:slug>_arret<int:pk>',
+    path('<slug:slug>_arret<int:pk>.pdf',
          views.arret_pdf, name='arret_pdf'),
         ]
 #    path('articles/<int:year>/', views.year_archive),
