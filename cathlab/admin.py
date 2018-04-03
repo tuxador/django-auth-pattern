cf370ed218983485624f97fd938ab3e4ae69d006
Yahyaoui Kaddour <kaddourkardio@gmail.com>
2018-04-03 09:25:50 +0100

    correction num telephone

--- cathlab/admin.py
+++ cathlab/admin.py
@@ -5,11 +5,14 @@ import datetime
 from .models import *
 
 # Register your models here.
+
+
 class CoronarographieAdmin(admin.ModelAdmin):
 
-        list_display = ("patient","intervention_date","operateur", "emergency")
+        list_display = ("patient", "intervention_date", "operateur", "emergency")
         list_filter = ('patient', 'intervention_date', 'operateur')
 #        search_fields = ('procedure','decision')
+
 
 class StimulationAdmin(admin.ModelAdmin):
 
@@ -17,8 +20,18 @@ class StimulationAdmin(admin.ModelAdmin):
         list_filter = ('patient', 'intervention_date', 'operateur')
 #        search_fields = ('name','phone')
 
+
+class CoroscanAdmin(admin.ModelAdmin):
+
+        list_display = ("patient", "coroscan_date", "medecin",
+                        "motif", "conclusion")
+        list_filter = ('patient', 'coroscan_date', 'medecin')
+#        search_fields = ('procedure','decision')
+
+
 admin.site.register(Coronarographie, CoronarographieAdmin)
 admin.site.register(Stimulation, StimulationAdmin)
+admin.site.register(Coroscan, CoroscanAdmin)
 admin.site.register(Lesion)
 admin.site.register(Indication)
 admin.site.register(Complication)
